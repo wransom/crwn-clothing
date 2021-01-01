89a40985b056716aafbe73e2bab682bd766c3452
William Ransom <wbransom@hotmail.com>
2021-01-01 16:48:31 -0700

    first update

---
+++ src/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className='directory menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>Hats</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
