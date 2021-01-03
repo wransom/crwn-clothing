24ee839d9897c08c64a0f5eeeb795368270f7a2a
William Ransom <wbransom@hotmail.com>
2021-01-03 10:28:44 -0700

    begin homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -7,7 +7,31 @@ const HomePage = () => {
                 <div className='menu-item'>
                     <div className='content'>
                         <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>Hats</span>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MEN</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMEN</h1>
+                        <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
             </div>
