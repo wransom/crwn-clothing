7eecfdb639fc5ae6e1345f103f7cac0758dd9062
William Ransom <wbransom@hotmail.com>
2021-01-17 11:55:45 -0700

    add signin and signup functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 import './App.css';
 
@@ -21,10 +21,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            // console.log(this.state);
+          })
+        });
+      }
+      this.setState({currentUser: userAuth})
+      //createUserProfileDocument(user);
+      // this.setState({ currentUser: user})
+
     });
   }
 
